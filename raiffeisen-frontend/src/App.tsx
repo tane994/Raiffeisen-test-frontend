c6a66ff138bac88af80aaf2234f80e3d7b54f321
Andrés Tanesini <andres.tanesini94@gmail.com>
2024-10-20 17:48:05 +0200

    feat: format notes data

--- raiffeisen-frontend/src/App.tsx
+++ raiffeisen-frontend/src/App.tsx
@@ -1,10 +1,34 @@
+import { useState, useEffect } from "react";
+import NoteInputForm from "./Components/NoteInputForm";
+import NoteList from "./Components/NoteList";
+import axios from "axios";
+import { Typography } from "@mui/material";
 
 function App() {
+  const [notesData, setNotesData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8081/api/v1/notes")
+      .then((response) => {
+        setNotesData(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setError(error.message);
+      });
+  }, []);
+
   return (
     <div>
-
+      <Typography variant="h2">Raiffeisen Note App</Typography>
+      <NoteInputForm />
+      <br />
+      <NoteList notesData={notesData} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- raiffeisen-frontend/src/Components/NoteList.tsx
+++ raiffeisen-frontend/src/Components/NoteList.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import {
+  Typography,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@mui/material";
 
-const NoteList = () => {
-  return <div>NoteList</div>;
+const NoteList = ({ notesData }) => {
+  return (
+    <div>
+      <Typography variant="h3">List of notes:</Typography>
+      <TableContainer>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Title</TableCell>
+              <TableCell align="center">Content</TableCell>
+              <TableCell align="center">Created</TableCell>
+              <TableCell align="center">Updated</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {notesData.map((note) => (
+              <TableRow
+                key={note.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="left">{note.title}</TableCell>
+                <TableCell align="left">{note.content}</TableCell>
+                <TableCell align="left">{note.createdAt}</TableCell>
+                <TableCell align="left">{note.updateAt}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 };
 
 export default NoteList;
