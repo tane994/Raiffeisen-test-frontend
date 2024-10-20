ae516bc947a55514256a28a6bf4e4b081a7d0f62
Andrés Tanesini <andres.tanesini94@gmail.com>
2024-10-20 18:11:07 +0200

    feat: use accordion to hide list of notes

--- raiffeisen-frontend/src/Components/NoteInputForm.tsx
+++ raiffeisen-frontend/src/Components/NoteInputForm.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { useState } from "react";
+import { TextField, Button } from "@mui/material";
 
 const NoteInputForm = () => {
-  return <div>NoteInputForm</div>;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  return (
+    <div>
+      <TextField
+        className="text-field"
+        label="Title"
+        variant="outlined"
+        //value={movieName}
+        onChange={(e) => setTitle(e.target.value)}
+        sx={{ width: "100%", mt: 1, mb: 1, fontFamily: "Roboto" }}
+        required
+      />
+      <TextField
+        className="text-field"
+        label="Content"
+        variant="outlined"
+        multiline
+        rows={4}
+        //value={review}
+        onChange={(e) => setContent(e.target.value)}
+        sx={{ width: "100%", mt: 1, mb: 1, fontFamily: "Baskerville" }}
+        required
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        sx={{ mt: 2, width: "100%" }}
+      >
+        Save Note
+      </Button>
+    </div>
+  );
 };
 
 export default NoteInputForm;
--- raiffeisen-frontend/src/Components/NoteList.tsx
+++ raiffeisen-frontend/src/Components/NoteList.tsx
@@ -7,37 +7,47 @@ import {
   TableRow,
   TableCell,
   TableBody,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
 } from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 const NoteList = ({ notesData }) => {
   return (
     <div>
-      <Typography variant="h3">List of notes:</Typography>
-      <TableContainer>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">Title</TableCell>
-              <TableCell align="center">Content</TableCell>
-              <TableCell align="center">Created</TableCell>
-              <TableCell align="center">Updated</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {notesData.map((note) => (
-              <TableRow
-                key={note.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell align="left">{note.title}</TableCell>
-                <TableCell align="left">{note.content}</TableCell>
-                <TableCell align="left">{note.createdAt}</TableCell>
-                <TableCell align="left">{note.updateAt}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Accordion>
+        <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+          <Typography variant="h4">List of Notes</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <TableContainer>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Title</TableCell>
+                  <TableCell align="center">Content</TableCell>
+                  <TableCell align="center">Created</TableCell>
+                  <TableCell align="center">Updated</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {notesData.map((note) => (
+                  <TableRow
+                    key={note.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell align="left">{note.title}</TableCell>
+                    <TableCell align="left">{note.content}</TableCell>
+                    <TableCell align="left">{note.createdAt}</TableCell>
+                    <TableCell align="left">{note.updateAt}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 };
